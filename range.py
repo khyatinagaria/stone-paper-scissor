9fe69e3574dbf8d298b51aa08efdc140c5c909e8
khyatinagaria <146091358+khyatinagaria@users.noreply.github.com>
2024-10-03 19:07:51 +0530

    Add files via upload

---
+++ range.py
@@ -0,0 +1,16 @@
+# seq = range(10)
+# for i in seq:
+#      print(i)
+
+# # to range in even number 
+# for i in range(2,100,2):
+#     print(i)
+
+# for i in range(100,0,-1):
+#     print(i)
+
+
+##############PASS STATEMENT########
+for el in range(10):
+    pass
+print("some useful work")
